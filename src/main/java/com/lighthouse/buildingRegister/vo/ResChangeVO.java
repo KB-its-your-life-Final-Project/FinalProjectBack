59b67e909dee54f1ffc69ac3a0dd262dfd2d9b0f
cheonkio <heelun8525@naver.com>
2025-07-18 11:32:00 +0900

    fix: 건축물 대장 변경 이력 VO 수정
    변경 날짜가 빈 문자열로 들어오는 경우가 있어 DATE 타입으로 저장하지 못하는 오류 발생. setter 직접 정의하여, 빈 문자열은 null로 변경

--- src/main/java/com/lighthouse/buildingRegister/vo/ResChangeVO.java
+++ src/main/java/com/lighthouse/buildingRegister/vo/ResChangeVO.java
@@ -4,13 +4,26 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
 public class ResChangeVO {
+    @Setter
     private Long registerId;
     private String resChangeDate;     // 변경일자
+    @Setter
     private String resChangeReason;   // 변경사유
+    @Setter
     private String resNote;           // 비고
     private String resIssueDate;      // 발급일자
+    @Setter
     private String resIssueOgzNm;     // 발급기관
+    @Setter
     private String resOriGinalData;   // PDF 파일 Base64값
+
+    public void setResChangeDate(String resChangeDate) {
+        this.resChangeDate = (resChangeDate == null || resChangeDate.trim().isEmpty()) ? null : resChangeDate;
+    }
+
+    public void setResIssueDate(String resIssueDate) {
+        this.resIssueDate = (resIssueDate == null || resIssueDate.trim().isEmpty()) ? null : resIssueDate;
+    }
+
 }
