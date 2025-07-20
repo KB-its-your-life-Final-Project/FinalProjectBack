41f336af21eb49748a206a58e06e9da320eeb7a6
laeongmulti <laeongmulticampus@gmail.com>
2025-07-20 22:05:07 +0900

    FEAT: 백엔드 API 통신체계 구축

--- src/main/java/com/lighthouse/member/controller/MemberController.java
+++ src/main/java/com/lighthouse/member/controller/MemberController.java
@@ -4,6 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import com.lighthouse.member.dto.MemberDTO;
 import com.lighthouse.member.service.MemberService;
+import com.lighthouse.response.ApiResponse;
+import com.lighthouse.response.ErrorCode;
+import com.lighthouse.response.SuccessCode;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,25 +16,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/member")
 @RequiredArgsConstructor
 @Slf4j
 public class MemberController {
-    private final MemberService memberService;
-    @GetMapping("")
-    public ResponseEntity<List<MemberDTO>> finaAll() {
-        return ResponseEntity.ok().body(memberService.findAll());
+    final MemberService memberService;
+
+    //모든 사용자 리스트
+    @GetMapping("/")
+    public ResponseEntity<ApiResponse<List<MemberDTO>>> findAll() {
+        List<MemberDTO> dtos = memberService.findAll();
+        return ResponseEntity.ok()
+                .body(ApiResponse.success(SuccessCode.MEMBER_FETCH_SUCCESS, dtos));
     }
 
-    @GetMapping("/{username}")
-    public ResponseEntity<MemberDTO> selectMemberByUSername(@PathVariable String username) {
-        MemberDTO dto = memberService.selectMemberByUsername(username);
-        if (dto == null) {
-            return ResponseEntity.notFound().build(); // 명시적 처리
+    //사용자 아이디로 찾기
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<MemberDTO>> findById(@PathVariable Number id) {
+        try {
+            MemberDTO dto = memberService.findById(id);
+            return ResponseEntity.ok()
+                    .body(ApiResponse.success(SuccessCode.MEMBER_FETCH_SUCCESS, dto));
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(ApiResponse.error(ErrorCode.MEMBER_NOT_FOUND));
         }
-        return ResponseEntity.ok().body(dto);
     }
 
 }
