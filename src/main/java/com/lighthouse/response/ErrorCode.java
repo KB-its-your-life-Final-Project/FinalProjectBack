ff24992cf7fd343ef61c7cc9152a379bb4f78902
BOOK-N6JF33997F\User <happy4u@kakao.com>
2025-07-30 14:15:34 +0900

    Feat: localinfo 호출 성공/실패

--- src/main/java/com/lighthouse/response/ErrorCode.java
+++ src/main/java/com/lighthouse/response/ErrorCode.java
@@ -10,8 +10,11 @@ public enum ErrorCode implements ResponseCode{
     // Member
     MEMBER_NOT_FOUND(1001, "회원 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     DUPLICATE_EMAIL(1002, "이미 사용 중인 이메일입니다.", HttpStatus.CONFLICT),
-    INVALID_PHONE_FORMAT(1003, "올바르지 않는 전화번호 형식입니다.", HttpStatus.BAD_REQUEST);
+    INVALID_PHONE_FORMAT(1003, "올바르지 않는 전화번호 형식입니다.", HttpStatus.BAD_REQUEST),
 
+    // Weather
+    WEATHER_FETCH_FAILED(500, "날씨 정보 조회 실패", HttpStatus.INTERNAL_SERVER_ERROR),
+    REGION_NOT_FOUND(404, "지역 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
     private final int code;
     private final String message;
     private final HttpStatus status;
--- src/main/java/com/lighthouse/response/SuccessCode.java
+++ src/main/java/com/lighthouse/response/SuccessCode.java
@@ -8,8 +8,10 @@ public enum SuccessCode implements ResponseCode{
 	// Member
 	MEMBER_FETCH_SUCCESS(100, "회원 조회 성공"),
 	MEMBER_CREATE_SUCCESS(101, "회원 가입 성공"),
-	MEMBER_UPDATE_SUCCESS(102, "회원 정보 수정 성공");
-	
-	private final int code;
+	MEMBER_UPDATE_SUCCESS(102, "회원 정보 수정 성공"),
+
+    //Weather
+    LOCALINFO_FETCH_SUCCESS(200, "지역 정보 조회 성공");
+    private final int code;
 	private final String message;
 }
